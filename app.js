ec6a9cb8217d88fa037b33e7a4386057d137f592
patleserdev <leserpat@hotmail.com>
2024-06-28 17:24:36 +0200

    add dot env at line 1

--- app.js
+++ app.js
@@ -1,10 +1,10 @@
+require('dotenv').config();
 var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
 require('./models/connection');
-require('dotenv').config();
 var indexRouter = require('./routes/index');
 var weatherRouter = require('./routes/weather');
 var userRouter = require('./routes/users');
